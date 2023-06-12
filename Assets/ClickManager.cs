4659228a75873745e40ace8a18cb8c8e80f4d2c0
Jonathan Uhler <jonathan@uhler.com>
2023-06-12 16:27:44 -0700

    remove unneeded variables

--- Assets/ClickManager.cs
+++ Assets/ClickManager.cs
@@ -2,27 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickManager : MonoBehaviour
-{
-    bool clicking=false;
+public class ClickManager : MonoBehaviour {
+	
     public GameObject blueTower;
+
+	
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0)){
-            clicking=true;
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
             Vector3 v3 = Input.mousePosition;
  		    v3.z = 10.0f;
  		    v3 = Camera.main.ScreenToWorldPoint(v3);
             Instantiate(blueTower,v3,Quaternion.identity);
-        }else{
-            clicking=false;
         }
     }
    

89e5327491fcd08b9de0846600b67bde5112f0c0
Jonathan Uhler <jonathan@uhler.com>
2023-06-13 14:04:52 -0700

    buff trishot

--- Assets/PurpleTower.cs
+++ Assets/PurpleTower.cs
@@ -24,7 +24,7 @@ public class PurpleTower : Tower {
     public override IEnumerator shoot() {
 		yield return new WaitForSeconds(this.getShootingCooldown());
 		if(base.enemyToShoot && this.inRange()) {
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < 5; i++) {
 				Quaternion rotation = this.transform.rotation;
 				rotation.z += UnityEngine.Random.Range(-0.5f, 0.5f);
 				GameObject bulletObj = Instantiate(base.bullet,

e77b9bbef21aa014a2b01cd59de2c5be75f19176
TheMagicApple <90540132+TheMagicApple@users.noreply.github.com>
2023-06-13 13:09:33 -0700

    cant place tower near other towers

--- Assets/ClickManager.cs
+++ Assets/ClickManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class ClickManager : MonoBehaviour {
 	
     public GameObject blueTower;
@@ -10,9 +10,12 @@ public class ClickManager : MonoBehaviour {
     public GameObject purpleTower;
 	
     GameObject[] towers;
+
+    List<GameObject> towersPlaced=new List<GameObject>();
     // Start is called before the first frame update
     void Start() {
         towers=new GameObject[]{blueTower,greenTower,orangeTower,purpleTower};
+        
     }
 
     // Update is called once per frame
@@ -21,8 +24,19 @@ public class ClickManager : MonoBehaviour {
             Vector3 v3 = Input.mousePosition;
  		    v3.z = 10.0f;
  		    v3 = Camera.main.ScreenToWorldPoint(v3);
-            Instantiate(towers[ButtonManager.towerPlacing-1],v3,Quaternion.identity);
-            ButtonManager.towerPlacing=0;
+            bool bad=false;
+            foreach(GameObject tower in towersPlaced){
+                double distance = Math.Sqrt(Math.Pow(v3.x - tower.transform.position.x, 2) + Math.Pow(v3.y - tower.transform.position.y, 2));
+                if(distance<1f){
+                    bad=true;
+                    break;
+                }
+            }
+            if(!bad){
+                towersPlaced.Add(Instantiate(towers[ButtonManager.towerPlacing-1],v3,Quaternion.identity));
+                ButtonManager.towerPlacing=0;
+            }
+            
             
         }
     }
